7b552d30b844e44e68e2c37019bddfae2bc4e7bc
Mike Armando <mike.armando.m@gmail.com>
2023-06-27 16:26:50 -0700

    Update

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,16 @@
+import './navbar.css'
+
+export const Navbar = () => {
+    return (
+        <div className='Navbar'>
+            <nav>
+                <h1>Marisco Bahia</h1>
+                <ul>
+                    <li>temp1</li>
+                    <li>temp2</li>
+                    <li>temp3</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
