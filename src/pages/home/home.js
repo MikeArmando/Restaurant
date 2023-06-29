76228636b20022624d0b5350b849f2392f004aff
Mike Armando <mike.armando.m@gmail.com>
2023-06-29 05:30:18 -0700

    Update

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -11,7 +11,7 @@ export const Home = () => {
 
       <section className='about-box'>
         <div className='about-box-center'>
-          <img src='https://images.unsplash.com/photo-1625631980634-397b9e9a73f9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGNvb2t8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'></img>
+          <img src='https://images.unsplash.com/photo-1625631980634-397b9e9a73f9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80' alt=''></img>
           <div className='about-text'>
             <h2>Mariscos Bahia <br /> <label> Restaurant</label></h2>
             <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth</p>
@@ -20,72 +20,88 @@ export const Home = () => {
       </section>
 
       <section className='menu-box'>
-        <h2>Our menu</h2>
+        <h2>Our Menu</h2>
         <div className='menu-center'>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
           <div className='menu-img-box'>
-            <img src='https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'></img>
+            <img src='https://images.unsplash.com/photo-1625943553852-781c6dd46faa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fGZpc2glMjBmb29kfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt=''></img>
             <div>
               <h3>Grilled Beef with potatoes</h3>
               <label>Meat, Potatoes, Rice, Tomatoe</label>
+              <p>$29</p>
             </div>
           </div>
         </div>
+        <div className='view-menu-box'>
+          <button><a href='#!'>View Menu</a></button>
+        </div>
+      </section>
+
+      <section className='reserv-box'>
+
       </section>
     </div>
   )
